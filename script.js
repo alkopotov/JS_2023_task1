aebd99fc25d1507a1bb410f187d14b275fb687d4
AlHobb <118611357+alkopotov@users.noreply.github.com>
2023-02-03 19:12:11 +0300

    Not for points but for understanding

--- script.js
+++ script.js
@@ -3,29 +3,27 @@ for (let i = 1; i <=30; i++) {
   arrayRandom.push(getNumber(0, 99))
 }
 const setRow = function (array) {
-  let row = ''
+  let row = '<tr>'
   for (i in array) {
     if (array[i] >= 50) {
       cl = 'orange';
     } else cl = 'black'
-    row += `<td class=${cl}> ${array[i]}</td>`
+    row += `<td class="${cl}">${array[i]}</td>`
   }
-  row = '<tr>' + row + '</tr>'
+  row += '</tr>';
   return row;
 }
 
-const setTable = function(array) {
+const setTable = function(array, row = 5, col = 6) {
   let str = '';
-  // for (i = 1; i <= 6; i ++) {
-  //   a = (i - 1) * 6
-  //   b = i * 6 
-  //   str += setRow(array.slice(a, b))
-  // }
-  str += setRow(array.slice(0, 6))
-  str += setRow(array.slice(6, 12))
-  str += setRow(array.slice(12, 18))
-  str += setRow(array.slice(18, 24))
-  str += setRow(array.slice(24, 30))
+  for (j = 0; j < row; j++) {
+    str += setRow(array.slice(j * col, (j + 1) * col))
+  }
+  // str += setRow(array.slice(0, 6))
+  // str += setRow(array.slice(6, 12))
+  // str += setRow(array.slice(12, 18))
+  // str += setRow(array.slice(18, 24))
+  // str += setRow(array.slice(24, 30))
   return str
 }
 
@@ -34,24 +32,22 @@ table = document.firstElementChild.lastElementChild.firstElementChild.nextElemen
 // console.log(setRow([1, 4]))
 
 table.innerHTML = setTable(arrayRandom);
-console.log(table.innerText)
-currentArr = table.innerText.split()
-console.log(currentArr);
-console.log(currentArr.length)
+// console.log(table.innerText)
+// currentArr = table.innerText.split()
+// console.log(currentArr);
+// console.log(currentArr.length)
 
 // let d = new RegExp('td')
 // return table.innerHTML.match(d).length
-const addElement = function(){
+const addElement = function() {
   a = getNumber(0, 99)
   if (a >= 50) {
     cl = "orange"
   } else cl = "black"
   if (arrayRandom.length % 6 == 0) {
-    table.firstElementChild.innerHTML += `<tr><td class=${cl}>${a}</td></tr>`
+    table.firstElementChild.innerHTML += `<tr><td class="${cl}">${a}</td></tr>`
   }  else {
-    table.firstElementChild.lastElementChild.innerHTML += `<td class=${cl}>${a}</td>`
+    table.firstElementChild.lastElementChild.innerHTML += `<td class="${cl}">${a}</td>`
   }
   arrayRandom.push(a)
 }
-
-
